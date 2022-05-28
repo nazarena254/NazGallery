c57bcd017d1840fde248bcb0d0d5a4a515bae524
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-28 13:11:33 +0300

    add Picture class in the models

--- picture/models.py
+++ picture/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -25,7 +26,7 @@ class Category(models.Model):
     def delete_category(self):
         self.save_category()
         self.delete()
-    def get_all(self):
+    def get_all_category(self):
         self.save_category()
     @classmethod
     def update_category(cls, id, category):
@@ -35,4 +36,38 @@ class Category(models.Model):
     def __str__(self):
         return self.category    
 
-    pass
+class Picture(models.Model):
+    image=models.ImageField(upload_to='pictures/')
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    location=models.ForeignKey('Location', on_delete=models.CASCADE)
+    category=models.ForeignKey('Category', on_delete=models.CASCADE)
+    posted_at=models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.save_image()
+        self.delete()
+    @classmethod
+    def update_image(cls,id,image):
+        image1=cls.objects.filter(id=id).update(image=image)
+        return image1
+    @classmethod
+    def get_image_by_id(cls,id):
+        image1=cls.objects.filter(id=id).all()
+        return image1
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Picture.objects.filter(location__location=location).all()
+        return image_location
+    @classmethod
+    def search_by_category(cls,search_term):
+        searched_images=cls.objects.filter(category__category__icontains=search_term)
+        return searched_images   
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering=['title']
