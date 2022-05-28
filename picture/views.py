4f8bc1e20eb319f92d6d3c2e7eec1cc42ea803ab
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-28 14:32:07 +0300

    add view functions to render html pages

--- picture/views.py
+++ picture/views.py
@@ -1,7 +1,24 @@
+from email import message
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Picture,Location,Category
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("<h1>welcome</h1>")
-    
+    pictures=Picture.objects.all()
+    kenya=Picture.filter_by_location(location='Kenya')
+    ethiopia=Picture.filter_by_location(location='Ethiopia')
+    rwanda=Picture.filter_by_location(location='Rwanda')
+    southafrica=Picture.filter_by_location(location='SouthAfrica')
+    return render(request, 'all-pictures/index.html', {'pictures':pictures,'Kenya':kenya,'Ethiopia':ethiopia,'Rwanda':rwanda,'SouthAfrica':southafrica})
+
+def search_reaults():
+    if 'picture' in request.GET and request.GET["picture"]:
+        search_term=request.GET.get("picture")
+        searched_pictures=Picture.search_by_category(search_term)
+        message=f"{search_term}"
+        return render(request, 'all-pictures/search.html',{"message":message,"pictures": searched_pictures})
+    else:
+        message = "You haven't searched for any picture"
+        return render(request, 'all-pictures/search.html',{"message":message})
