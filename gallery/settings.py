990c46d0b726578cb4af59d62ee51ac6caf4c653
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-28 18:16:37 +0300

    configure  the location for media in settings.py file

--- gallery/settings.py
+++ gallery/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+
 # development
 if config('MODE')=="dev":
    DATABASES = {
